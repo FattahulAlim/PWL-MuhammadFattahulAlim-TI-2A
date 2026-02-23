e9849cafb775793c8941160d65377cfb772302de
FattahulAlim <fathalim1512@gmail.com>
2026-02-23 10:17:12 +0700

    feat : add route

---
+++ Jobsheet2/routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/hello', function() {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+   return 'World';
+});
+
+Route::get('/about', function () {
+   return 'Nim: 244107020018 <br> Nama: Muhammad Fattahul Alim';
+});
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '. $name;
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel Dengan ID '. $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '. $name;
+});
+
+
