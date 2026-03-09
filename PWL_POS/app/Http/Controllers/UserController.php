36a6fd52e56b177082c1c659f06d6227ce816fd7
FattahulAlim <fathalim1512@gmail.com>
2026-03-09 14:53:34 +0700

    refactor: update user data retrieval and display in UserController and user view

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,8 +10,15 @@
     {
         public function index()
         {
-            $user = UserModel::where('level_id', 2)->count();
-            // dd($user);
+            $user = UserModel::firstOrNew([
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => hash::make('12345'),
+                'level_id' => 2
+            ]);
+
+            $user->save();
+
             return view('user', ['data' => $user]);
         }
 
