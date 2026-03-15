533513f210a340f2ca87807689a034c300c326b0
FattahulAlim <fathalim1512@gmail.com>
2026-03-15 12:50:03 +0700

    refactor: clean up UserController formatting and update user creation logic

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -1,25 +1,31 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Hash;
-    use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\UserModel;
 
-    class UserController extends Controller
+class UserController extends Controller
+{
+    public function index()
     {
-        public function index()
-        {
-            $user = UserModel::firstOrNew([
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => hash::make('12345'),
-                'level_id' => 2
-            ]);
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-            $user->save();
+        $user->username = 'manager12';
 
-            return view('user', ['data' => $user]);
-        }
+        $user->save();
 
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
+
+}
